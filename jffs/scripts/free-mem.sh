1b34e079d386aab84484caa84d5a3927ae50e347
Mao Yongjun <yongjun.mao@outlook.com>
2020-04-17 20:53:16 +0800

    add a schedule to restart wan
    Restart wan every two days, and then no longer need restart router.
    Fix free-mem KB to MB calc issue

--- jffs/scripts/free-mem.sh
+++ jffs/scripts/free-mem.sh
@@ -2,13 +2,13 @@
 
 gate=80
 
-cur=`free | grep Mem: | awk '{print int($4/1e3)}'`
+cur=`free | grep Mem: | awk '{print int($4/1024)}'`
 logger "Current free memory: "$cur"M"
 
 if [[ $cur -lt $gate ]];then
   logger "Start drop caches..."
   sync && echo 3 > /proc/sys/vm/drop_caches
-  cur=`free | grep Mem: | awk '{print int($4/1e3)}'`
+  cur=`free | grep Mem: | awk '{print int($4/1024)}'`
   logger "Done, Now free memory: "$cur"M"
 fi
 
